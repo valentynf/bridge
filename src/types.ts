5280230df155d35bf22dd07a05e2dd311b227d8a
valentynf <valentynf@wix.com>
2026-04-10 18:06:13 +0200

    adding types

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,35 @@ type CardSuit = "hearts" | "spades" | "diamonds" | "clubs"
 
 type CardRank = "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A"
 
-export type Card = {
+type PendingEffect = "TAKE_1" | "SKIP_TURN"
+
+type SpecialAction = {
+    targetIndex: number
+    effect: PendingEffect[]
+}
+
+type Card = {
     rank: CardRank
     suit: CardSuit
+}
+
+type Player = {
+    nickname: string
+    score: number
+    hand: Card[]
+    isEliminated: boolean
+}
+
+type GamePhase = "PLAYING" | "ROUND_OVER" | "GAME_OVER"
+
+export type BridgeGameState = {
+    currentPhase: GamePhase
+    players: Player[]
+    currentDealerIndex: number
+    currentPlayerIndex: number
+    drawPile: Card[]
+    activePile: Card[]
+    jackSuit: CardSuit | undefined
+    pendingSpecialEffects: SpecialAction[]
+    reshuffleCount: number
 }
