4cfe827a8054c7a4ed12da9621c0f2720684eba4
valentynf <valentynf@wix.com>
2026-08-02 16:36:45 +0200

    fixed destructuring issue

--- server/src/functions/jwt.ts
+++ server/src/functions/jwt.ts
@@ -1,13 +1,13 @@
-import { sign, verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 type TokenPayload = { userId: string };
 
 export const signToken = (payload: TokenPayload) => {
-    return sign(payload, JWT_SECRET, { expiresIn: "7d" });
+    return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
 };
 
 export const verifyToken = (token: string): TokenPayload => {
-    return verify(token, JWT_SECRET) as TokenPayload;
+    return jwt.verify(token, JWT_SECRET) as TokenPayload;
 };
